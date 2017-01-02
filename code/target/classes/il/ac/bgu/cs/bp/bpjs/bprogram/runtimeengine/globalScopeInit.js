42e949a7e6fac1aba1fb40c04e1c5ade0172be4b
Michael Bar-Sinai <mich.barsinai@gmail.com>
2017-01-02 22:37:54 +0200

    Setups. travis

--- code/target/classes/il/ac/bgu/cs/bp/bpjs/bprogram/runtimeengine/globalScopeInit.js
+++
@@ -1,7 +0,0 @@
-/* global package imports, variables, etc. */
-
-//importPackage(Packages.bp);
-//importPackage(Packages.bp.eventsets);
-
-//importPackage(Packages.bp.exceptions);
-//java.lang.System.out.println("evaluated globalScopeInit");
