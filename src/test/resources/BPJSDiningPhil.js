cfaa39453c5bedda8baf8dcede703771343649e4
geraw <gera.weiss@gmail.com>
2017-08-01 11:52:05 +0300

    Added labels to all bSyncs

--- src/test/resources/BPJSDiningPhil.js
+++ src/test/resources/BPJSDiningPhil.js
@@ -3,55 +3,66 @@ N = 5;
 
 bp.log.info('Dinning philosophers with ' + N + ' philosophers')
 
-
 addStick = function(i) {
 	var j = (i % N) + 1;
 
-	bp.registerBThread("Stick " + i,
-			function() {
-				while (true) {
-					var e = bsync({
+	bp.registerBThread("Stick " + i, function() {
+		
+		var e = "";
+		var wt = "";
+		
+		while (true) {
+			e = bsync({
 						waitFor : [ bp.Event("Pick" + i + "R"),
-								bp.Event("Pick" + j + "L") ],
+								    bp.Event("Pick" + j + "L") ],
 
-						block : [ bp.Event("Rel" + i + "R"), 
-								bp.Event("Rel" + j + "L") ], }).name;
+						block : [ bp.Event("Rel" + i + "R"),
+			  					  bp.Event("Rel" + j + "L") ],
+			  					  
+					}, "1 " + wt + e).name;
+			
+			
+			wt = (e.equals("Pick" + i + "R")) ? "Rel" + i + "R" : "Rel" + j + "L";
 
-					var wt = (e.equals("Pick" + i + "R")) ? "Rel" + i + "R"
-							: "Rel" + j + "L";
+			bsync({
+				waitFor : bp.Event(wt),
 
-					bsync({
-						waitFor : bp.Event(wt),
+				block : [ bp.Event("Pick" + i + "R"),
+						  bp.Event("Pick" + j + "L") ]	
+			
+			}, "2 " + wt + e);
 
-						block : [ bp.Event("Pick" + i + "R"),
-								bp.Event("Pick" + j + "L") ] });
-					
-					wt = "";
-					e = "";
-				}
-			});
+			wt = "";
+			e = "";
+		}
+	});
 };
-
-
 
 addPhil = function(i) {
 	bp.registerBThread("Phil" + i, function() {
 		while (true) {
 			// Request to pick the right stick
-			bsync({ request : bp.Event("Pick" + i + "R") });
+			bsync({
+				request : bp.Event("Pick" + i + "R")
+			},"1");
 
 			// Request to pick the left stick
-			bsync({ request : bp.Event("Pick" + i + "L") });
+			bsync({
+				request : bp.Event("Pick" + i + "L")
+			},"2");
 
 			// Request to release the left stick
-			bsync({ request : bp.Event("Rel" + i + "L") });
+			bsync({
+				request : bp.Event("Rel" + i + "L")
+			}, "3");
 
 			// Request to release the right stick
-			bsync({ request : bp.Event("Rel" + i + "R") });
+			bsync({
+				request : bp.Event("Rel" + i + "R")
+			}, "4");
 		}
 	});
 };
-
 
 for (i = 1; i <= N; i++) {
 	addStick(i);
