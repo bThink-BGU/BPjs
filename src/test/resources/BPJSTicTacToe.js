852e6c36d8d1cde1e8136e0562f3c0d3925352ed
geraw <gera.weiss@gmail.com>
2017-09-03 15:22:06 +0300

    .

--- src/test/resources/BPJSTicTacToe.js
+++ src/test/resources/BPJSTicTacToe.js
@@ -54,7 +54,6 @@ bp.registerBThread("EnforceTurns", function() {
 		});
 
 		bsync({
-
 			waitFor : [ O(0, 0), O(0, 1), O(0, 2), O(1, 0), O(1, 1), O(1, 2), O(2, 0), O(2, 1), O(2, 2) ],
 
 			block : [ X(0, 0), X(0, 1), X(0, 2), X(1, 0), X(1, 1), X(1, 2), X(2, 0), X(2, 1), X(2, 2) ]
