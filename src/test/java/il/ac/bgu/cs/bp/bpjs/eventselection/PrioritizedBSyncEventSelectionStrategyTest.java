a7b66f9be88421aec63f9edeb9a823cdb34ede67
reututy <reutete1@gmail.com>
2017-11-12 13:38:47 +0200

    Added 2 tamplates for Junitest

---
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/eventselection/PrioritizedBSyncEventSelectionStrategyTest.java
@@ -0,0 +1,54 @@
+package il.ac.bgu.cs.bp.bpjs.eventselection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrioritizedBSyncEventSelectionStrategyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPrioritizedBSyncEventSelectionStrategyLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrioritizedBSyncEventSelectionStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectableEvents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractEventSelectionStrategyLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractEventSelectionStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRequestedAndNotBlocked() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/eventselection/PrioritizedBThreadsEventSelectionStrategyTest.java
@@ -0,0 +1,74 @@
+package il.ac.bgu.cs.bp.bpjs.eventselection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrioritizedBThreadsEventSelectionStrategyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetSeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrioritizedBThreadsEventSelectionStrategyLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrioritizedBThreadsEventSelectionStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectableEvents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHighestPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLowestPriority() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractEventSelectionStrategyLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAbstractEventSelectionStrategy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRequestedAndNotBlocked() {
+		fail("Not yet implemented");
+	}
+
+}
