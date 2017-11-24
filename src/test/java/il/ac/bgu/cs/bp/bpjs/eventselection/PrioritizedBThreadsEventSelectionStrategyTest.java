7aa57cb35932fc8201307e2b19f6b8baffd3f268
reututy <reutete1@gmail.com>
2017-11-24 13:27:42 +0200

    PBsyncESST works

--- src/test/java/il/ac/bgu/cs/bp/bpjs/eventselection/PrioritizedBThreadsEventSelectionStrategyTest.java
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/eventselection/PrioritizedBThreadsEventSelectionStrategyTest.java
@@ -2,75 +2,114 @@ package il.ac.bgu.cs.bp.bpjs.eventselection;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import il.ac.bgu.cs.bp.bpjs.bprogram.runtimeengine.BSyncStatement;
+import il.ac.bgu.cs.bp.bpjs.events.BEvent;
 
 @Ignore
 public class PrioritizedBThreadsEventSelectionStrategyTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+//	private static final BEvent evt1 = new BEvent("evt1");
+//	private static final BEvent evt2 = new BEvent("evt2");
+//	private static final BEvent evt3 = new BEvent("evt3");
+//	private static final BEvent evt4 = new BEvent("evt4");
+//	
+//	@Test
+//	public void testSelectableEvents_noBlocking() throws InterruptedException {
+//		PrioritizedBThreadsEventSelectionStrategy sut = new PrioritizedBThreadsEventSelectionStrategy();
+//
+//		Set<BSyncStatement> stmts = new HashSet<>();
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt4)));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt1)).data(5));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt2)).data(10));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt3)).data(10));
+//
+//		assertEquals(new HashSet<>(Arrays.asList(evt2, evt3)), //
+//				sut.selectableEvents(stmts, Collections.emptyList()));
+//	}
+//
+//	@Test
+//	
+//	public void testSelectableEvents_withBlocking() {
+//		PrioritizedBThreadsEventSelectionStrategy sut = new PrioritizedBThreadsEventSelectionStrategy();
+//
+//		Set<BSyncStatement> stmts = new HashSet<>();
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt4)));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt1)).data(5));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt2)).data(10));
+//		stmts.add(BSyncStatement.make().request(Arrays.asList(evt3)).data(10).block(evt2));
+//
+//		assertEquals(new HashSet<>(Arrays.asList(evt3)), //
+//				sut.selectableEvents(stmts, Collections.emptyList()));
+//	}
+	
+//	@Test
+//	public void testPrioritizedBThreadsEventSelectionStrategy() {
+//		fail("Not yet implemented");
+//	}
 
-	@Test
-	public void testGetSeed() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testPrioritizedBThreadsEventSelectionStrategyLong() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testGetSeed() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testPrioritizedBThreadsEventSelectionStrategyLong() {
+//		fail("Not yet implemented");
+//	}
+//
 
-	@Test
-	public void testPrioritizedBThreadsEventSelectionStrategy() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSelectableEvents() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetPriority() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetPriority() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetHighestPriority() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetLowestPriority() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAbstractEventSelectionStrategyLong() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAbstractEventSelectionStrategy() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSelect() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetRequestedAndNotBlocked() {
-		fail("Not yet implemented");
-	}
+//
+//	@Test
+//	public void testSelectableEvents() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetPriority() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetPriority() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetHighestPriority() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetLowestPriority() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAbstractEventSelectionStrategyLong() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAbstractEventSelectionStrategy() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSelect() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetRequestedAndNotBlocked() {
+//		fail("Not yet implemented");
+//	}
 
 }
