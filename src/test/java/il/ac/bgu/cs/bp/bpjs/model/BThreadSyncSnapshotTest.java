b7db971c4404be655c083e0036f283723a75fff5
Ace Pace <acepace@gmail.com>
2018-04-14 17:56:34 +0300

    Add test to check for variable inequality between snapshot. currently fails.

--- src/test/java/il/ac/bgu/cs/bp/bpjs/model/BThreadSyncSnapshotTest.java
+++ src/test/java/il/ac/bgu/cs/bp/bpjs/model/BThreadSyncSnapshotTest.java
@@ -23,7 +23,18 @@
  */
 package il.ac.bgu.cs.bp.bpjs.model;
 
+import il.ac.bgu.cs.bp.bpjs.execution.listeners.BProgramRunnerListener;
+import il.ac.bgu.cs.bp.bpjs.internal.ExecutorServiceMaker;
+import il.ac.bgu.cs.bp.bpjs.model.eventselection.EventSelectionResult;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+
 import static org.junit.Assert.*;
 
 /**
@@ -40,6 +51,39 @@ public class BThreadSyncSnapshotTest {
         BThreadSyncSnapshot sut = new BThreadSyncSnapshot();
         assertEquals( BThreadSyncSnapshot.class.getName(), sut.getName() );
     }
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    private final List<BProgramRunnerListener> listeners = new ArrayList<>();
+
+    @Test
+    public void testJSVarState() throws InterruptedException {
+        BProgram bprog = new SingleResourceBProgram("SnapshotTests/ABCDTrace.js");
+        BProgramSyncSnapshot setup = bprog.setup();
+
+        ExecutorService execSvc = ExecutorServiceMaker.makeWithName("BProgramSnapshotEqualityTest");
+        List<BProgramSyncSnapshot> snapshots = new ArrayList<>();
+        BProgramSyncSnapshot step = setup.start(execSvc);
+        snapshots.add(step);
+        //Iteration 1,starts already at request state A
+        for (int i =0; i< 4; i++) {
+            Set<BEvent> possibleEvents_a = bprog.getEventSelectionStrategy().selectableEvents(step.getStatements(), step.getExternalEvents());
+            EventSelectionResult event_a = bprog.getEventSelectionStrategy().select(step.getStatements(), step.getExternalEvents(), possibleEvents_a).get();
+            step = step.triggerEvent(event_a.getEvent(), execSvc, listeners);
+        }
+        snapshots.add(step);
+        for (int i =0; i< 4; i++) {
+            Set<BEvent> possibleEvents_a = bprog.getEventSelectionStrategy().selectableEvents(step.getStatements(), step.getExternalEvents());
+            EventSelectionResult event_a = bprog.getEventSelectionStrategy().select(step.getStatements(), step.getExternalEvents(), possibleEvents_a).get();
+            step = step.triggerEvent(event_a.getEvent(), execSvc, listeners);
+        }
+        snapshots.add(step);
+        //these two objects (by debugging) share program counter and frame index and should differ on variables only
+        assertNotEquals(snapshots.get(1).getBThreadSnapshots(),snapshots.get(2).getBThreadSnapshots());
+    }
+
+
     
 }
 
