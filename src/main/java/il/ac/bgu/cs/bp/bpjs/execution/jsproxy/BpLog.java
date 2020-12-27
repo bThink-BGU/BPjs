73af0e941fc4df308fd33bd4e0c4ed62da29c723
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2020-12-27 16:36:28 +0200

    Bug fix in MapProxy::filter

--- src/main/java/il/ac/bgu/cs/bp/bpjs/execution/jsproxy/BpLog.java
+++ src/main/java/il/ac/bgu/cs/bp/bpjs/execution/jsproxy/BpLog.java
@@ -23,6 +23,7 @@
  */
 package il.ac.bgu.cs.bp.bpjs.execution.jsproxy;
 
+import il.ac.bgu.cs.bp.bpjs.internal.ScriptableUtils;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import java.util.Arrays;
 import java.util.List;
@@ -60,7 +61,7 @@ public class BpLog implements java.io.Serializable {
 
     public void log(LogLevel lvl, Object msg) {
         if (level.compareTo(lvl) >= 0) {
-            System.out.println("[BP][" + lvl.name() + "] " + stringify(msg));
+            System.out.println("[BP][" + lvl.name() + "] " + ScriptableUtils.stringify(msg));
         }
     }
 
@@ -74,36 +75,6 @@ public class BpLog implements java.io.Serializable {
         return level.name();
     }
     
-    private String stringify( Object o ) {
-        if ( o == null ) return "<null>";
-        if ( o instanceof String ) return "\""+o + "\"";
-        if ( o instanceof ConsString ) return "\"" + ((ConsString)o).toString() + "\"";
-        if ( o instanceof NativeArray ) {
-            NativeArray arr = (NativeArray) o;
-            return arr.getIndexIds().stream().map( id -> id + ":" + stringify(arr.get(id)) ).collect(joining(" | ", "[JS_Array ", "]"));
-        }
-        if ( o instanceof ScriptableObject ) {
-            ScriptableObject sob = (ScriptableObject) o;
-            return Arrays.stream(sob.getIds()).map( id -> id + ": " + stringify(sob.get(id)) ).collect( joining("\n", "{JS_Obj ", "}"));
-        }
-        if ( o instanceof Object[] ) {
-            Object[] objArr = (Object[]) o;
-            return IntStream.range(0, objArr.length).mapToObj(idx -> stringify(objArr[idx])).collect(joining("|","[Java_Array ", "]"));
-        }
-        if ( o instanceof Map ) {
-            Map<?,?> mp = (Map<?,?>) o;
-            return mp.entrySet().stream().map( e -> e.getKey()+"->" + stringify(e.getValue())).collect(joining(",","{Map ", "}"));
-        }
-        if ( o instanceof List ) {
-            List<?> ls = (List<?>) o;
-            return ls.stream().map(this::stringify).collect(joining(", ","<List ", ">"));
-        }
-        if ( o instanceof Set ) {
-            Set<?> ls = (Set<?>) o;
-            return ls.stream().map(this::stringify).collect(joining(", ","{Set ", "}"));
-        }
-        return o.toString();
-        
-    }
+
     
 }
