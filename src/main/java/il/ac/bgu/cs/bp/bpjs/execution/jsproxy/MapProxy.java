37f376f6eb0c0f2fa72bb8347ff92c49b3895e15
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-10-26 17:01:05 +0300

    Added values function to MapProxy.java

--- src/main/java/il/ac/bgu/cs/bp/bpjs/execution/jsproxy/MapProxy.java
+++ src/main/java/il/ac/bgu/cs/bp/bpjs/execution/jsproxy/MapProxy.java
@@ -128,6 +128,10 @@ public class MapProxy<K,V> implements java.io.Serializable {
         }
     }
 
+    public Collection<V> values() {
+        return filter((k, v) -> true).values();
+    }
+
     public Set<K> keys() {
         Set<K> retVal = new HashSet<>(seed.keySet());
         modifications.forEach((k,v)->{
