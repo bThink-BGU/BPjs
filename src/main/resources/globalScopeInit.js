a8e258fbf9b56823a4a332a59e35eb78aec18c20
Coderx <c0d3r.x0@gmail.com>
2026-04-03 05:10:15 +0000

    fix: wrap bp.log methods to add dummy param and lazy stringify (#241)
    - Always append a dummy {} to log call args to prevent the
      single-array-param Java varargs misinterpretation issue
    - JSON.stringify object params only when the log level is active
    - Implemented as a JS wrapper in globalScopeInit.js, loaded at
      program scope initialization in BProgram.java

--- src/main/resources/globalScopeInit.js
+++ src/main/resources/globalScopeInit.js
@@ -1,9 +1,35 @@
-/* global package imports, variables, etc., bp */
+/* global bp */
+// Wrap bp.log methods to:
+// 1. Add a dummy {} at the end to prevent the single-array-param Java varargs issue
+// 2. JSON.stringify params lazily (only when the log level is active)
+(function() {
+    var _javaLog = bp.log;
 
-// /!\ Currently deactivated, as we have nothing to add here yet. Re-activate in BProgram's setup() method.
+    function _wrapArgs(args) {
+        var result = [args[0]];
+        for (var i = 1; i < args.length; i++) {
+            var p = args[i];
+            if (p !== null && p !== undefined && typeof p === 'object') {
+                try { p = JSON.stringify(p); } catch(e) {}
+            }
+            result.push(p);
+        }
+        result.push({});
+        return result;
+    }
 
-//importPackage(Packages.bp);
-//importPackage(Packages.bp.eventsets);
+    bp.log = {
+        setLevel:             function(lvl) { _javaLog.setLevel(lvl); },
+        setLoggerPrintStream: function(ps)  { _javaLog.setLoggerPrintStream(ps); },
+        getLevel:             function()    { return _javaLog.getLevel(); },
+        isFineEnabled:        function()    { return _javaLog.isFineEnabled(); },
+        isInfoEnabled:        function()    { return _javaLog.isInfoEnabled(); },
+        isWarnEnabled:        function()    { return _javaLog.isWarnEnabled(); },
+        isErrorEnabled:       function()    { return _javaLog.isErrorEnabled(); },
 
-//importPackage(Packages.bp.exceptions);
-
+        fine:  function() { if (_javaLog.isFineEnabled())  _javaLog.fine.apply( _javaLog, _wrapArgs(arguments)); },
+        info:  function() { if (_javaLog.isInfoEnabled())  _javaLog.info.apply( _javaLog, _wrapArgs(arguments)); },
+        warn:  function() { if (_javaLog.isWarnEnabled())  _javaLog.warn.apply( _javaLog, _wrapArgs(arguments)); },
+        error: function() { if (_javaLog.isErrorEnabled()) _javaLog.error.apply(_javaLog, _wrapArgs(arguments)); }
+    };
+})();
